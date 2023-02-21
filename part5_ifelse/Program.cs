ab29c36b80d2e7bad90b72a791ade93ae235d47a
Ekaterina Cherkas <ekaterina.cherkas12@mail.ru>
2023-02-21 21:02:19 +0300

    add if else

---
+++ part5_ifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("YYYYHY , its Masha");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
